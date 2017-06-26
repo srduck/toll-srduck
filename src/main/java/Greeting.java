2528136edb96953e8c42a3260eaabfc91872d96c
srduck-local <srdkr@yandex.ru>
2017-06-27 02:37:24 +0700

    добавление класса Greeting

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,8 @@
+/**
+ * Created by igor on 27.06.2017.
+ */
+public class Greeting {
+    public static void main (String... args){
+        System.out.println("Слушатель курса \"Профессиональная разработка на JAVA\"!");
+    }
+}
