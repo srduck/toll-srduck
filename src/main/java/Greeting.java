574f68287ce7556786845cc4d96198557db6f2a9
srduck-local <srduck@yandex.ru>
2017-07-02 19:55:32 +0700

    безжалостное удаление SRC

--- src/main/java/Greeting.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by igor on 02.07.2017.
- */
-public class Greeting {
-    public static void main (String... args){
-        System.out.println("Слушатель курса \"Профессиональная разработка на JAVA\"!");
-    }
-}
-
