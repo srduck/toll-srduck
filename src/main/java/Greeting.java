bca69c77bb8a6cbc6e12b6fefd2338eb1772be9e
srduck-local <srduck@yandex.ru>
2017-07-02 19:53:47 +0700

    добавление класса Greeting в build-dependency

---
+++ src/main/java/Greeting.java
@@ -0,0 +1,9 @@
+/**
+ * Created by igor on 02.07.2017.
+ */
+public class Greeting {
+    public static void main (String... args){
+        System.out.println("Слушатель курса \"Профессиональная разработка на JAVA\"!");
+    }
+}
+
