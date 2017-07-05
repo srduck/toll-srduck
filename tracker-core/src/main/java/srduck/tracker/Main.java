c17527727164c66ca2bf4a0f95ed0dd313e7ae08
srduck-local <srduck@yandex.ru>
2017-07-06 02:22:52 +0700

    добавдение класса Main для демонстрации запуска

---
+++ tracker-core/src/main/java/srduck/tracker/Main.java
@@ -0,0 +1,18 @@
+package srduck.tracker;
+
+import srduck.dto.PointDTO;
+
+/**
+ * Created by igor on 06.07.2017.
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        for (int i=0; i<5; i++) {
+            System.out.println("Main.main say Hello!!!!");
+            PointDTO point = new PointDTO();
+            point.setLat(45);
+            System.out.println(point.toJson());
+            Thread.sleep(1000);
+        }
+    }
+}
