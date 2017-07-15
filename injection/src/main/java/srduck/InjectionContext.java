74b3afe0d6bccb1fb2a556f01766e7f2d3726c37
srduck-local <srduck@yandex.ru>
2017-07-15 18:14:38 +0700

    добавление контекста

---
+++ injection/src/main/java/srduck/InjectionContext.java
@@ -0,0 +1,10 @@
+package srduck;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by igor on 15.07.2017.
+ */
+@Configuration
+public class InjectionContext {
+}
--- injection/src/main/java/srduck/services/DataSendService.java
+++ injection/src/main/java/srduck/services/DataSendService.java
@@ -1,8 +1,10 @@
+package srduck.services;
+
 import org.springframework.stereotype.Service;
 
 /**
  * Created by igor on 15.07.2017.
  */
 @Service
-public class DataPeekService {
+public class DataSendService {
 }
