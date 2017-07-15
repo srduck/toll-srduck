2f34d1ebf6cb5ff450455e855e55592a7793d588
srduck-local <srduck@yandex.ru>
2017-07-15 18:27:10 +0700

    иньекция зависимости

--- injection/src/main/java/srduck/services/DataPeekService.java
+++ injection/src/main/java/srduck/services/DataPeekService.java
@@ -1,5 +1,6 @@
 package srduck.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,4 +8,9 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DataPeekService {
+
+    @Autowired
+    private DataSendService dataSendService;
+
 }
+
