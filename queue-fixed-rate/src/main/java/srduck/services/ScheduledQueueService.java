33db2c2761eab490a03ef02527d6c1538d87a425
srduck-local <srduck@yandex.ru>
2017-07-16 19:01:04 +0700

    логгирование

--- queue-fixed-rate/src/main/java/srduck/services/ScheduledQueueService.java
+++ queue-fixed-rate/src/main/java/srduck/services/ScheduledQueueService.java
@@ -1,5 +1,7 @@
 package srduck.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,21 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class ScheduledQueueService {
 
+    private static Logger log = LoggerFactory.getLogger(ScheduledQueueService.class);
+
     BlockingDeque<String> queue = new LinkedBlockingDeque<>(100);
     private int putCount;
     private long previous;
 
     @Scheduled(fixedDelay = 1000)
     private void take() throws InterruptedException {
-        System.out.println("take trying!");
+        //System.out.println("take trying!");
+        log.info("take trying!");
         long current = System.currentTimeMillis();
+//        log.info((current - previous) + " ScheduledQueueService.take " + queue.poll(500, TimeUnit.MILLISECONDS));
 //        System.out.println( (current - previous) + " ScheduledQueueService.take " + queue.poll(500, TimeUnit.MILLISECONDS));
-        System.out.println( (current - previous) + " ScheduledQueueService.take " + queue.take());
+        log.info( (current - previous) + " ScheduledQueueService.take " + queue.take());
+//        System.out.println( (current - previous) + " ScheduledQueueService.take " + queue.take());
         previous = current;
 
     }
@@ -30,7 +37,8 @@ public class ScheduledQueueService {
     @Scheduled(fixedDelay = 5000)
     private void put() throws InterruptedException {
         int i = putCount++;
-        System.out.println("ScheduledQueueService.put " + i);
+        log.info("ScheduledQueueService.put " + i);
+//        System.out.println("ScheduledQueueService.put " + i);
         queue.put("new string => " + i);
 
     }
