3cc8dd4172175c9ceec22482543fbe18f1e4232f
srduck-local <srduck@yandex.ru>
2017-08-16 00:07:44 +0700

    unit tests for counter controller, + mock for relay

--- mvc-boot/src/test/java/controllers/CounterControllerTest.java
+++ mvc-boot/src/test/java/controllers/CounterControllerTest.java
@@ -2,12 +2,19 @@ package controllers;
 
 import domain.Country;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by igor on 15.08.2017.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class CounterControllerTest {
     @Test
     public void greeting() throws Exception {
@@ -26,9 +33,8 @@ public class CounterControllerTest {
     @Test
     public void getCount() throws Exception {
         CounterController controller = new CounterController();
-        Long count = controller.getCount();
+        assertEquals(1,(long) controller.getCount());
         assertEquals(2,(long) controller.getCount());
-        assertEquals(3,(long) controller.getCount());
     }
 
     @Test
@@ -45,10 +51,16 @@ public class CounterControllerTest {
         assertEquals(false, response.isSuccess());
     }
 
+    @Mock
+    RestTemplate restTemplate;
+    @InjectMocks
+    CounterController mockedController;
+
     @Test
     public void relay() throws Exception {
-        CounterController controller = new CounterController();
-        Country country = controller.relay();
+        when(restTemplate.getForObject("http://services.groupkt.com/country/get/iso2code/RU", Country.class)).thenReturn(new Country());
+        Country country = mockedController.relay();
+        assertNotNull(country);
     }
 
 }
