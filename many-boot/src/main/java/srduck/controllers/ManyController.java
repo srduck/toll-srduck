461fa394ae0502fdacb3eea2e307151ed44e4b13
Igor Serdyukov <srduckr@yandex.ru>
2017-09-08 16:15:59 +0700

    Иллюстрация гонок в веб-приложении с AtomicInteger

--- many-boot/src/main/java/srduck/controllers/ManyController.java
+++ many-boot/src/main/java/srduck/controllers/ManyController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by main on 08.09.17.
  */
@@ -13,16 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ManyController {
 
     Logger log = LoggerFactory.getLogger(ManyController.class);
-    Object lock = new Object();
-    private int counter;
+
+    private AtomicInteger counter = new AtomicInteger();
 
     @RequestMapping("/increment")
     public State increment(){
         log.info(Thread.currentThread().getName() + " /increment");
         for (int i = 0; i < 10000; i++){
-            synchronized (lock) {
-                counter++;
-            }
+            counter.incrementAndGet();
         }
         return new State("ok");
     }
@@ -30,13 +30,14 @@ public class ManyController {
     @RequestMapping("/get")
     public int get(){
         log.info(Thread.currentThread().getName() + " /get");
-        return counter;
+        return counter.get();
     }
 
     @RequestMapping("/set")
     public int set(@RequestParam(value="value") int value){
         log.info(Thread.currentThread().getName() + " /set");
-        return counter = value;
+        counter.set(value);
+        return counter.get();
     }
 
 }
